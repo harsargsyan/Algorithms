f187ca3a86bdc3a248aed5c31305e6bab88a0e24
harsargsyan <harsargsyan@gmail.com>
2022-08-17 14:18:36 +0400

    implemented selection sort algorithm

--- Algorithms/Sorting/SelectionSort.cs
+++ Algorithms/Sorting/SelectionSort.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithms.Sorting
+﻿namespace Algorithms.Sorting
 {
     internal class SelectionSort
     {
+        static void SortSelection(List<int> li)
+        {
+            for (int i = 0; i < li.Count; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < li.Count; j++)
+                {
+                    if (li[j] < li[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                Extencion.Swap(li, minIndex, i);
+            }
+        }
+        
+        static void Main()
+        {
+            List<int> list = new List<int>() { 5, 1, 3, 8, 4, 0, 7, 9 };
+            SortSelection(list);
+            foreach (int i in list)
+            {
+                Console.Write($" {i} ");
+            }
+        }
+    }
+
+    static class Extencion
+    {
+        internal static void Swap<T>(this List<T> list, int i, int j)
+        {
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
     }
 }
