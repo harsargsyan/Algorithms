c6d20909849d39ec0ed8e11e4eb528ea3eb77fa1
harsargsyan <harsargsyan@gmail.com>
2022-08-25 09:31:51 +0400

    start merge sort

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -6,12 +6,14 @@ namespace Algorithms
     {
         static void Main()
         {
-            List<int> list = new List<int>() { 5, 1, 3, 2, 8, 4, 0, 7, 6, 9 };
+            List<int> list = new List<int>() { 1,5,8 };
+            List<int> list2 = new List<int>() { 3,4,6,9 };
             //Selection.SelectionSort(list);
             //Bubble.BubbleSort(list);
             //Insertion.InsertionSort(list);
-            Counting.CountingSort(list);
-            foreach (int i in list)
+            //Counting.CountingSort(list);
+            List<int> merged = Merge.MergeLists(list, list2);
+            foreach (int i in merged)
             {
                 Console.Write($" {i} ");
             }
---
+++ Algorithms/Sorting/Merge.cs
@@ -0,0 +1,34 @@
+﻿namespace Algorithms.Sorting
+{
+    /**
+     * time complexity of this sort is
+     * there are log2(n) levels
+     * in each level we do n steps
+     * total for the best/worst cases: O(n*Log2(n))
+     */
+    class Merge
+    {
+        internal static void MergeSort(List<int> li)
+        {
+
+        }
+
+        internal static List<int> MergeLists(List<int> l1, List<int> l2)
+        {
+            List<int> merged = new List<int>();
+            int i = 0, j = 0;
+            while (i < l1.Count && j < l2.Count)
+            {
+                if (l1[i] < l2[j])
+                {
+                    merged.Add(l1[i++]);
+                }
+                else
+                {
+                    merged.Add(l2[j++]);
+                }
+            }
+            return merged;
+        }
+    }
+}
