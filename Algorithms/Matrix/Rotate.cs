bc0e2a880163e9cc5b9bc29d0c3e96596ba79f25
harutyun-s <harutyun@zealous.tech>
2025-05-10 13:00:58 +0400

    added rotate matrix algorithm

---
+++ Algorithms/Matrix/Rotate.cs
@@ -0,0 +1,23 @@
+﻿namespace Algorithms.Matrix;
+
+public class Rotate
+{
+    // Time Complexity — O(n × m)
+    public static byte[,] Rotate90Clockwise(byte[,] input)
+    {
+        int height = input.GetLength(0);
+        int width = input.GetLength(1);
+
+        byte[,] rotated = new byte[width, height];
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                rotated[j, height - 1 - i] = input[i, j];
+            }
+        }
+
+        return rotated;
+    }
+}
