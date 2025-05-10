be7961ad25d943362f14f64a596855445861a26d
harutyun-s <harutyun@zealous.tech>
2025-05-10 13:51:27 +0400

    added image resize

---
+++ Algorithms/Matrix/ResizeImage.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms.Matrix;
+
+public class ResizeImage
+{
+    public static byte[,] ResizeNearestNeighbor(byte[,] input, int newHeight, int newWidth)
+    {
+        int originalHeight = input.GetLength(0);
+        int originalWidth = input.GetLength(1);
+
+        byte[,] resized = new byte[newHeight, newWidth];
+
+        for (int i = 0; i < newHeight; i++)
+        {
+            for (int j = 0; j < newWidth; j++)
+            {
+                int origI = (int)((i / (double)newHeight) * originalHeight);
+                int origJ = (int)((j / (double)newWidth) * originalWidth);
+                
+                origI = Math.Min(origI, originalHeight - 1);
+                origJ = Math.Min(origJ, originalWidth - 1);
+
+                resized[i, j] = input[origI, origJ];
+            }
+        }
+
+        return resized;
+    }
+}
